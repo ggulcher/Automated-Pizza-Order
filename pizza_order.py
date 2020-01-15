96b51b52f84bb66eaa9252d8595844dde4003b01
Griffin <58752175+SunsetSuperman@users.noreply.github.com>
2020-01-15 17:00:35 -0500

    Update pizza_order.py

--- pizza_order.py
+++ pizza_order.py
@@ -2,7 +2,10 @@ from pizzapi import *
 
 customer = Customer("Sunset", "Superman", "sunsetsuperman@gmail.com", "8005550123")
 address = Address("1158 Yo Mama St", "Columbus", "OH", "43210")
+
 store = address.closest_store()
+
+menu = store.get_menu()
 
 order = Order(store, customer, address)
 order.add_item('P12IPAZA')
@@ -10,14 +13,8 @@ order.add_item('20BDCOKE')
 
 card = PaymentObject("1234567890123456", "1234", "777", "43210")
 
-def order_pizza(order, card):
-    order.place(card)
+# To place order
+# order.place(card)
 
-command = input("Your usual order, sir?")
-
-if command == "yes":
-    print("Of course, sir.")
-    order_pizza(order)
-else:
-    print("Of course, sir. Shutting down.")
-    exit()
+# To test
+# order.pay_with(card) 
